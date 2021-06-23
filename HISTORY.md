939be0ad8f18588a4162586ff33d2dcc16dfbe05
wang.kangning <wb-whm546427@alibaba-inc.com>
2021-06-23 10:19:35 +0800

    - update: version of `classname` - update: esm support

---
+++ HISTORY.md
@@ -0,0 +1,6 @@
+# ChangeLog
+
+## 0.0.5 / 2021-6-23
+
+- update: version of `classname`
+- update: esm support
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import classnames from 'classnames';
-import { ClassNamesFn, ClassValue } from 'classnames/types';
+import classnames, { Argument } from 'classnames';
 
 /**
  * add prefix to every `classnames` arguments
@@ -7,10 +6,10 @@ import { ClassNamesFn, ClassValue } from 'classnames/types';
  * @param {string} seperator
  * @return {string} plain className
  */
-const classPrefix = (CLS_PREFIX: string = '', seperator: string = '-'): ClassNamesFn => {
+const classPrefix = (CLS_PREFIX: string = '', seperator: string = '-'): typeof classnames => {
   const addPrefix = (cls: string | number) => (!!CLS_PREFIX && !!cls ? `${CLS_PREFIX}${seperator}${cls}` : cls);
 
-  const loop = (arg: ClassValue): ClassValue => {
+  const loop = (arg: Argument): Argument => {
     if (typeof arg === 'string' || typeof arg === 'number') {
       return addPrefix(arg);
     } else if (Array.isArray(arg) && arg.length) {
@@ -25,7 +24,7 @@ const classPrefix = (CLS_PREFIX: string = '', seperator: string = '-'): ClassNam
     return arg;
   };
 
-  return (...args: ClassValue[]): string => {
+  return (...args: Argument[]): string => {
     if (args.length === 0) {
       return CLS_PREFIX;
     }
